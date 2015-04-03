19a8fad5a089bf4283d23965be22117687d11dc7
forrest <14351834@qq.com>
2015-04-03 10:41:56 +0800

    Initial Commit

---
+++ SpringCareManage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpringCareManage
+//
+//  Created by forrestLee on 4/3/15.
+//  Copyright (c) 2015 cmkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
